4ef81bbb8d8a3e591521942c13bcd70ac2a97272
Juan Culebras <juancubello@gmail.com>
2012-04-17 12:47:07 +0200

    Layout con boton de ejemplo añadido

--- gen/com/proyect/android/R.java
+++ gen/com/proyect/android/R.java
@@ -32,8 +32,9 @@ public final class R {
     }
     public static final class layout {
         public static final int main=0x7f030000;
-        public static final int main_con_estilos=0x7f030001;
-        public static final int main_con_relative=0x7f030002;
+        public static final int main_con_boton=0x7f030001;
+        public static final int main_con_estilos=0x7f030002;
+        public static final int main_con_relative=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f050001;
--- src/com/proyect/android/GitProyectAndroidActivity.java
+++ src/com/proyect/android/GitProyectAndroidActivity.java
@@ -2,6 +2,8 @@ package com.proyect.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class GitProyectAndroidActivity extends Activity {
     /** Called when the activity is first created. */
@@ -10,6 +12,11 @@ public class GitProyectAndroidActivity extends Activity {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.main);
         //setContentView(R.layout.main_con_relative);
-        setContentView(R.layout.main_con_estilos);
+        //setContentView(R.layout.main_con_estilos);
+        setContentView(R.layout.main_con_boton);
+    }
+    
+    public void click(View view){
+    	Log.e("CLICK_BOTON", "Esta aplicaci—n se autodestrir‡ en 5 segundos :) ");
     }
 }
