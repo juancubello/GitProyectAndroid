1b841f3ef4e25e207ad52659ee5ba239a1c44330
Juan Culebras <juancubello@gmail.com>
2012-04-17 12:03:48 +0200

    Ejercicios Layouts lineales y relativas

--- gen/com/proyect/android/R.java
+++ gen/com/proyect/android/R.java
@@ -13,11 +13,24 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int text1=0x7f050000;
+        public static final int text2=0x7f050001;
+        public static final int text3=0x7f050002;
+        public static final int text4=0x7f050003;
+        public static final int text5=0x7f050004;
+        public static final int text6=0x7f050005;
+        public static final int text7=0x7f050006;
+        public static final int text8=0x7f050007;
+        public static final int text9=0x7f050008;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int main_con_relative=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int textocelda=0x7f040002;
     }
 }
